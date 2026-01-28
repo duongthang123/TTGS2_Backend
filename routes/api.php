c09bdf517fd8282e76df98a3d7ccd7e961e061ba
duongthang123 <88389413+duongthang123@users.noreply.github.com>
2026-01-28 22:16:52 +0700

    init project

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/hello', function () {
+    return response()->json([
+        'message' => 'Chào mừng bạn đến với Backend Laravel!',
+        'status' => 'success'
+    ]);
+});
